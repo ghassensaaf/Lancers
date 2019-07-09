9f9d791474ff4e17aacdb5bfb5a1f33b5384fd13
ghassensaaf <44534175+ghassensaaf@users.noreply.github.com>
2019-07-09 09:01:27 +0200

    Update contactform.php

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,18 +1,19 @@
 <?php
   $name = $_POST['name'];
-  $email_from = $_POST['email'];
+  $email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
+  $email_from='info@website.com';
   $email_subject = "$subject";
   $email_body =   "User name: $name. \n".
                   "User Email: $email. \n".
-                  "Message: $message. \n"
+                  "Message: $message. \n";
 
   $to = "saafghassen@gmail.com";
-  $headers = "From: $email_from. \r\n";
+  $headers = "From: $email. \r\n";
   $headers = "Reply-to : $email_from \r\n";
-  
+
   mail($to,$email_subject,$email_body,$headers);
   header("Location: index.html");
 ?>
